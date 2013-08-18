803cee466010cc8b7335aa4cab2296a508c28bcc
unknown <srinisen@SRINISEN1.asiapacific.hpqcorp.net>
2013-08-18 20:29:36 +0530

    committed the First push

---
+++ js/jquery-ui-customSortable.js
@@ -0,0 +1,49 @@
+// JScript source code
+
+$.widget("ui.customsortable", $.extend({}, $.ui.sortable.prototype, {
+
+    _init: function() {
+        this.element.data('sortable', this.element.data('customsortable'));
+        return $.ui.sortable.prototype._init.apply(this, arguments);
+    },
+
+    // Override other methods here.
+    
+    _mouseStop: function(event, noPropagation) {
+
+		if(!event) {
+			return;
+		}
+
+		//If we are using droppables, inform the manager about the drop
+		if ($.ui.ddmanager && !this.options.dropBehaviour) {
+			$.ui.ddmanager.drop(this, event);
+		}
+
+		if(this.options.revert) {
+			var that = this,
+				cur = this.placeholder.offset(),
+				axis = this.options.axis,
+				animation = {};
+
+			if ( !axis || axis === "x" ) {
+				animation.left = cur.left - this.offset.parent.left - this.margins.left + (this.offsetParent[0] === document.body ? 0 : this.offsetParent[0].scrollLeft);
+			}
+			if ( !axis || axis === "y" ) {
+				animation.top = cur.top - this.offset.parent.top - this.margins.top + (this.offsetParent[0] === document.body ? 0 : this.offsetParent[0].scrollTop);
+			}
+			this.reverting = true;
+			$(this.helper).animate( animation, parseInt(this.options.revert, 10) || 500, function() {
+				that._clear(event);
+			});
+		} else {
+			this._clear(event, noPropagation);
+		}
+
+		return false;
+
+	}
+
+}));
+
+$.ui.customsortable.defaults = $.extend({}, $.ui.sortable.defaults);
